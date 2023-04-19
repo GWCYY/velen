5471b58eed8bfe94281a19367854cff099285d2d
流月忆 <15155065624@163.com>
2023-04-19 22:40:01 +0800

    添加测试用service

--- velen-service/src/main/java/com/fx/velenservice/controller/TestController.java
+++ velen-service/src/main/java/com/fx/velenservice/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.fx.velenservice.controller;
 
+import com.fx.velenservice.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final TestService testService;
+
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
     @GetMapping("/home")
     public String test(){
-        return "1025";
+        return testService.test();
     }
 }
---
+++ velen-service/src/main/java/com/fx/velenservice/service/TestService.java
@@ -0,0 +1,12 @@
+package com.fx.velenservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test() {
+        return "1025";
+    }
+
+}
